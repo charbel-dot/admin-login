e44ebfb9f3a742b543aa5c4d1405fdf7262eb1a7
charbel-dot <72027836+charbel-dot@users.noreply.github.com>
2023-08-09 16:15:08 +0300

    Adedd files

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const emailInput = document.querySelector("[data-email]");
+const passwordInput = document.querySelector("[data-password]");
+
+passwordInput.addEventListener("input", (e) => {
+  var dataInput = Array.from(e.target.value);
+});
+
+const displayMessage = () => {
+  return;
+};
